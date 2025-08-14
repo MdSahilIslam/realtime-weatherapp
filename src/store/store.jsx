347023241cc4509f0fa1de16fa76f38c07a59842
MdSahilIslam <islamsahil2001@gmail.com>
2025-08-14 23:10:44 +0530

    error message update 2

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -37,7 +37,7 @@ export const AppContextProvider = ({children}) => {
 
 
         }catch(e){
-            alert("This place does'n exist \n or maybe api request deadline /mont exceeded");
+            alert("This place does'n exist \n or maybe api request deadline /month exceeded");
         }
     }
 
